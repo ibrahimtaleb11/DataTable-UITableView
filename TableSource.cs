2cde59b98d6b34b0dc4eb12ab8220012316d085d
ibrahimtaleb11 <ibrahimtaleb11@hotmail.com>
2016-05-21 02:09:30 -0700

    uitableview

---
+++ TableSource.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UIKit;
+
+namespace ibrahimapp
+{
+	public class TableSource:UITableViewSource
+	{
+		string[]tableitems;
+		string cellidentifier="TableCell";
+
+		public TableSource (string[] items)
+		{
+			tableitems = items;
+
+		}
+
+		//retuen length table
+		public override nint RowsInSection (UITableView tableview, nint section)
+		{
+			return tableitems.Length;
+		}
+
+		public override void RowSelected (UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			new UIAlertView ("Alert", "You Click On" + tableitems [indexPath.Row], null, "ok", null).Show ();
+			tableView.DeselectRow (indexPath, true);
+		}
+		public override UITableViewCell GetCell (UITableView tableView, Foundation.NSIndexPath indexPath)
+		{
+			UITableViewCell cell = tableView.DequeueReusableCell (cellidentifier);
+			if (cell == null)
+				cell = new UITableViewCell (UITableViewCellStyle.Default, cellidentifier);
+			cell.TextLabel.Text = tableitems [indexPath.Row];
+			return cell;
+		}
+	}
+}
+
---
+++ ViewController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using UIKit;
+using System.Data;
+
+
+namespace ibrahimapp
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+			//string []data=new string[]{"ibrahim","taleb","mohammed","dahan"};
+
+			locatservice.WebServicetesting ws = new locatservice.WebServicetesting ();
+			ws.allusersAsync ();
+			ws.allusersCompleted += Handle_ServicegetDrugsCompleted;
+
+		
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+
+
+
+
+		        private void Handle_ServicegetDrugsCompleted(object sender,locatservice.allusersCompletedEventArgs args)
+				{
+					if(args.Result == null)
+						return;
+			        DataTable dt = new DataTable ();
+			        dt = args.Result;
+
+			       string[] data = new string[dt.Rows.Count];
+
+			for (int i = 0; i < dt.Rows.Count; i++) 
+			
+			{
+				data [i] = dt.Rows [i] [1].ToString ();
+
+
+			}
+			//show data in Table by Call TableSource.cs Class.
+			UITableView _table1;
+			_table1 = new UITableView {
+
+				Frame =new CoreGraphics.CGRect(0,20,View.Bounds.Width,View.Bounds.Height),
+				Source=new TableSource(data)
+			};
+			View.AddSubview(_table1);
+
+
+
+
+				}
+	}
+}
+
